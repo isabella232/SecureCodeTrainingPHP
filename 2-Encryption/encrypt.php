acf5d6b7bdb0b8f8ac032af2b2445297b01eefb0
jaysaini987 <jay@vtxsecurity.com>
2023-06-20 11:44:47 +1000

    update encryptor code

--- 2-Encryption/encrypt.php
+++ 2-Encryption/encrypt.php
@@ -1,48 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Text Encryption/Decryption</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <style>
-      body {
-        padding-top: 50px;
-      }
-      .container {
-        max-width: 500px;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <h1 class="text-center mb-4">Text Encryption/Decryption</h1>
-      <div class="card">
-        <div class="card-body">
-          <?php
-            // Encrypt
-            echo "<p>Weakly Encrypted Text is :";
-            $target = $_POST["encrypt"];
-            $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
-            $size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-            $iv = mcrypt_create_iv($size, MCRYPT_DEV_RANDOM);
-            $mcrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $target, MCRYPT_MODE_CBC, $iv);
-            echo $mcrypted;
-            echo "<br>";
 
-            // Decrypt
-            $decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $target, MCRYPT_MODE_CBC, $iv);
-            echo $decrypted;
+<head>
+  <title>Text Encryption/Decryption</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <style>
+    body {
+      padding-top: 50px;
+    }
 
-            echo "<br>";
+    .container {
+      max-width: 500px;
+    }
+  </style>
+</head>
 
-            echo "Strong Encrypted Text is :";
-            // Insert secure code here
-          ?>
-        </div>
+<body>
+  <div class="container">
+    <h1 class="text-center mb-4">Text Encryption/Decryption</h1>
+    <div class="card">
+      <div class="card-body">
+        <?php
+
+        function encryptText($input, $key)
+        {
+          $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('DES'));
+          $ciphertext = openssl_encrypt($input, 'DES', $key, OPENSSL_RAW_DATA, $iv);
+          $encryptedText = base64_encode($iv . $ciphertext);
+          return $encryptedText;
+        }
+
+        function decryptText($encryptedText, $key)
+        {
+          $data = base64_decode($encryptedText);
+          $iv = substr($data, 0, openssl_cipher_iv_length('DES'));
+          $ciphertext = substr($data, openssl_cipher_iv_length('DES'));
+          $decryptedText = openssl_decrypt($ciphertext, 'DES', $key, OPENSSL_RAW_DATA, $iv);
+          return $decryptedText;
+        }
+
+
+        $input = $_POST['encrypt']; // User input text
+        $key = 'mysecretpassword'; // Encryption key
+
+        $encryptedText = encryptText($input, $key);
+        echo "Input text: " . $input;
+        echo "<br>";
+        echo "Encrypted Text: " . $encryptedText;
+        echo "<br>";
+        $decryptedText = decryptText($encryptedText, $key);
+        echo "Decrypted text: " . $decryptedText;
+        ?>
       </div>
     </div>
+  </div>
 
-    <!-- Bootstrap JS -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  </body>
+  <!-- Bootstrap JS -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+
 </html>
